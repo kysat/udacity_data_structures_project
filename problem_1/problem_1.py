d31d21da26a377bd0502c15d75561d229552f18b
kysat <skt1523@gmail.com>
2021-05-15 23:58:59 +0900

    [add] implement problem_1

--- problem_1/problem_1.py
+++ problem_1/problem_1.py
@@ -1,16 +1,25 @@
+from collections import OrderedDict
+
 class LRU_Cache(object):
 
     def __init__(self, capacity):
         # Initialize class variables
-        pass
+        self.capacity = capacity
+        self.data = OrderedDict()
 
     def get(self, key):
         # Retrieve item from provided key. Return -1 if nonexistent.
-        pass
+        if key in self.data and self.data[key] is not None:
+            self.data.move_to_end(key)
+            return self.data[key]
+        return -1
 
     def set(self, key, value):
         # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
-        pass
+        if len(self.data) >= self.capacity:
+            self.data.popitem(last=False)
+        self.data[key] = value
+
 
 our_cache = LRU_Cache(5)
 
@@ -19,12 +28,11 @@ our_cache.set(2, 2)
 our_cache.set(3, 3)
 our_cache.set(4, 4)
 
-
-our_cache.get(1)       # returns 1
-our_cache.get(2)       # returns 2
-our_cache.get(9)      # returns -1 because 9 is not present in the cache
+print(our_cache.get(1))  # returns 1
+print(our_cache.get(2))  # returns 2
+print(our_cache.get(9))  # returns -1 because 9 is not present in the cache
 
 our_cache.set(5, 5)
 our_cache.set(6, 6)
 
-our_cache.get(3)      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+print(our_cache.get(3))  # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
