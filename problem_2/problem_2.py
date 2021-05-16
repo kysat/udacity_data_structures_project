5ab4cf673d62ed5c2abce2b964e61ad6418ffee5
kysat <skt1523@gmail.com>
2021-05-16 17:30:23 +0900

    [add] implement problem_2

--- problem_2/problem_2.py
+++ problem_2/problem_2.py
@@ -1,3 +1,5 @@
+import os
+
 def find_files(suffix, path):
     """
     Find all files beneath path with file name suffix.
@@ -14,7 +16,17 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
-    return None
+    return sorted(walk_and_find_files(suffix, path, list()))
 
 
+def walk_and_find_files(suffix, path, found_file_list):
+    file_list = os.listdir(path)
+    parent = path
+    for file_or_directory in file_list:
+        if os.path.isdir(os.path.join(parent, file_or_directory)):
+            walk_and_find_files(suffix, os.path.join(parent, file_or_directory), found_file_list)
+        elif file_or_directory.endswith(suffix):
+            found_file_list.append(os.path.join(parent, file_or_directory))
+    return found_file_list
 
+print(find_files('.c', '.'))
